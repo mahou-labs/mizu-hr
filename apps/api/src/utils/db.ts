57a15d71d866efad7fb89574a5c9ad008780fe48
Nicolas Chartiot <nchartiot@proton.me>
2025-08-17 22:27:51 -0300

    add hyperdrive to postgres connection

--- apps/api/src/utils/db.ts
+++ apps/api/src/utils/db.ts
@@ -1,4 +1,13 @@
 import { env } from "cloudflare:workers";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const getDb = () => drizzle(env.DATABASE_URL || "");
+export function getDb() {
+  const pool = new Pool({
+    connectionString: env.HYPERDRIVE.connectionString,
+    max: 5,
+  });
+
+  return drizzle(pool);
+}
+// export const getDb = () => drizzle(env.DATABASE_URL || "");
