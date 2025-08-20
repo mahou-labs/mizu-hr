6f10d6da7f087a22197f01096d0a3548cbec326a
Nicolas Chartiot <nchartiot@proton.me>
2025-08-19 21:58:23 -0300

    validate env vars for site

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 
 import cloudflare from "@astrojs/cloudflare";
-import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  env: {
+    schema: {
+      API_URL: envField.string({ context: "client", access: "public" }),
+    },
+  },
   adapter: cloudflare(),
   integrations: [react()],
 });
--- apps/site/src/utils/auth-client.ts
+++ apps/site/src/utils/auth-client.ts
@@ -1,5 +1,6 @@
+import { API_URL } from "astro:env/client";
 import { createAuthClientInstance } from "@mizu-hr/auth";
 
 export const authClient = createAuthClientInstance({
-  baseURL: "http://localhost:3000",
+  baseURL: API_URL,
 });
