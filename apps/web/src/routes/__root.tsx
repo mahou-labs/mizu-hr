c097c70d0a7ad49b732530fb99b97b8bd9785b2f
Nicolas Chartiot <nchartiot@proton.me>
2025-09-01 18:17:39 -0300

    fix dark and light mode loading

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -62,20 +62,21 @@ function RootDocument() {
     <html lang="en">
       <head>
         <HeadContent />
-        {/* <script
-          // biome-ignore lint/security/noDangerouslySetInnerHtml: dark mode script
-          dangerouslySetInnerHTML={{
-            __html: `;(()=>{try{var s=localStorage.getItem('theme');var q=window.matchMedia('(prefers-color-scheme: dark)');var d=s?s==='dark':q.matches;var r=document.documentElement;r.classList.toggle('dark',d);var m=document.createElement('meta');m.name='color-scheme';m.content=d?'dark light':'light dark';document.head.appendChild(m);if(!s&&q.addEventListener){q.addEventListener('change',e=>{r.classList.toggle('dark',e.matches)})}}catch(e){}})();`,
-          }}
-        /> */}
       </head>
       <body>
-        {/* <script
+        <script
           // biome-ignore lint/security/noDangerouslySetInnerHtml: dark mode script
           dangerouslySetInnerHTML={{
-            __html: `document.body.classList.toggle('dark',document.documentElement.classList.contains('dark'));`,
+            __html: `
+            let theme = localStorage.getItem('theme')
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+            if(theme === 'system' && prefersDark) {
+              theme = 'dark'
+            }
+            document.documentElement.classList.add(theme === 'dark' ? 'dark' : 'light');
+          `,
           }}
-        /> */}
+        />
         <div className="h-svh bg-sidebar">
           <Outlet />
         </div>
