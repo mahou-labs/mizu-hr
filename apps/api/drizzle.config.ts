5c5e171f9e4ed38caefeba9702141c1f4d8c12b8
Nicolas Chartiot <nchartiot@proton.me>
2025-09-02 22:09:56 -0300

    move drizzle migrations to another folder

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/schema",
-  out: "./src/drizzle/migrations",
+  out: "./src/drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL || "",
