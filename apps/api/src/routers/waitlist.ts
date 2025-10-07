8e2fad13a2dc434bfe3df0a3cbefd70be6a5ff79
Nicolas Chartiot <nchartiot@proton.me>
2025-10-07 18:01:57 -0300

    Add better error handling

--- apps/api/src/routers/waitlist.ts
+++ apps/api/src/routers/waitlist.ts
@@ -1,19 +1,26 @@
+import { ORPCError } from "@orpc/server";
 import z from "zod";
 import { waitlist } from "@/schema/waitlist";
 import { db } from "@/utils/db";
+import { tryCatch } from "@/utils/try-catch";
 import { publicProcedure } from "../utils/orpc";
 
 export const waitlistRouter = {
   addEmail: publicProcedure
     .input(z.object({ email: z.email() }))
     .handler(async ({ input }) => {
-      await db
-        .insert(waitlist)
-        .values({
-          email: input.email,
-        })
-        .onConflictDoNothing({ target: waitlist.email })
-        .returning();
+      const { error } = await tryCatch(
+        db
+          .insert(waitlist)
+          .values({
+            email: input.email,
+          })
+          .onConflictDoNothing({ target: waitlist.email })
+      );
+
+      if (error) {
+        throw new ORPCError("Failed to add email to waitlist");
+      }
 
       return { success: true };
     }),
