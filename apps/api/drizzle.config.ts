41fec96716c41f3d69d03445ec962e77bbff00fa
Nicolas Chartiot <nchartiot@proton.me>
2026-02-07 02:14:57 -0300

    Update ci job and drizzle config

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   schema: "./src/schema",
   out: "./src/drizzle",
   dialect: "postgresql",
+  schemaFilter: ["public"],
   dbCredentials: {
     url: process.env.DATABASE_URL || "",
     ssl: "verify-full",
